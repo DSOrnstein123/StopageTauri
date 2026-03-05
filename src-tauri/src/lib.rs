bb23bc8f6a35c24dff065a4a5851fce4d324f942
DSOrnstein <namphonggithub@gmail.com>
2026-03-06 00:11:35 +0700

    feat(be/flashcards): implement get cards and deck list query

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,56 @@
+use backend::{
+    database::migrate::migrate,
+    features::{
+        documents::repo::Document,
+        flashcards::{
+            cards::repo::Card,
+            decks::{repo::Deck, service},
+        },
+    },
+};
+use sqlx::{types::Uuid, SqlitePool};
+use tauri::{Manager, State};
+
+pub mod features;
+
+#[allow(dead_code)]
+pub struct AppState {
+    db: SqlitePool,
+}
+
 #[tauri::command]
-fn ping_backend() -> String {
-    backend::ping()
+async fn get_decks(state: State<'_, AppState>) -> Result<Vec<Deck>, String> {
+    service::get_decks(&state.db)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn get_documents_list(state: State<'_, AppState>) -> Result<Vec<Document>, String> {
+    backend::features::documents::repo::get_documents_list(&state.db)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn get_cards_from_deck(
+    state: State<'_, AppState>,
+    deck_id: Uuid,
+) -> Result<Vec<Card>, String> {
+    backend::features::flashcards::cards::repo::get_cards_from_deck(&state.db, deck_id)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn create_deck(
+    state: State<'_, AppState>,
+    name: String,
+    parent_id: Option<Uuid>,
+) -> Result<Deck, String> {
+    backend::features::flashcards::decks::repo::create_deck(&state.db, name, parent_id)
+        .await
+        .map_err(|e| e.to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -14,9 +64,24 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
+            tauri::async_runtime::block_on(async {
+                let db = backend::repo::pool::connect().await;
+                migrate(&db).await?;
+
+                app.manage(AppState { db });
+
+                Ok::<(), Box<dyn std::error::Error>>(())
+            })?;
+
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![ping_backend])
+        .invoke_handler(tauri::generate_handler![
+            get_documents_list,
+            get_decks,
+            get_cards_from_deck,
+            create_deck
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
