22a1b5e6a653478f3f09b4277776e981ee94a011
DSOrnstein <namphonggithub@gmail.com>
2026-03-06 00:07:48 +0700

    feat(be): set up tauri

---
+++ src-tauri/src/features/flashcards/handlers.rs
@@ -0,0 +1,11 @@
+// use backend::features::flashcards::decks::repo::Deck;
+// use tauri::State;
+
+// use crate::AppState;
+
+// // #[tauri::command]
+// // pub async fn get_decks_list(state: State<'_, AppState>) -> Result<Vec<Deck>, String> {
+// //     backend::features::flashcards::decks::repo::get_decks_list(&state.db)
+// //         .await
+// //         .map_err(|e| e.to_string())
+// // }
