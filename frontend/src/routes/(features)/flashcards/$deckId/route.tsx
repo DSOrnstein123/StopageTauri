148e35c6155457b1ed04e0a17c0e672dedd82a68
DSOrnstein <namphonggithub@gmail.com>
2026-03-21 14:51:16 +0700

    refactor(fe/flashcards): remove unused code

--- frontend/src/routes/(features)/flashcards/$deckId/route.tsx
+++ frontend/src/routes/(features)/flashcards/$deckId/route.tsx
@@ -1,8 +1,7 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { decksFetchQueryOption } from "../-hooks/useDecksFetch";
 
 export const Route = createFileRoute("/(features)/flashcards/$deckId")({
-  component: Outlet,
   loader: async ({ context: { queryClient }, params: { deckId } }) => {
     const decks = await queryClient.ensureQueryData(decksFetchQueryOption);
     const selectedDeck = decks.find((deck) => deck.id === deckId);
