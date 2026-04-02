d2ef356aeba71331c027855e927f49898cd03fc0
DSOrnstein <namphonggithub@gmail.com>
2026-04-02 23:49:45 +0700

    feat(fe/editor): set up table columns and rows

--- frontend/src/features/document/tiptap/extensions/database/DatabaseView.tsx
+++ frontend/src/features/document/tiptap/extensions/database/DatabaseView.tsx
@@ -1,37 +1,74 @@
 import { NodeViewWrapper } from "@tiptap/react";
-import { GripVertical } from "lucide-react";
+import { GripVertical, Trash } from "lucide-react";
 import {
   useReactTable,
   getCoreRowModel,
   flexRender,
+  createColumnHelper,
 } from "@tanstack/react-table";
 import { useMemo } from "react";
 
+interface ColumnValueMap {
+  text: string;
+  number: number;
+  date: string;
+  checkbox: boolean;
+}
+
+type ColumnType = keyof ColumnValueMap;
+
+interface Column {
+  id: string;
+  name: string;
+  type: ColumnType;
+}
+
+interface Row {
+  id: string;
+  properties: Record<string, unknown>;
+}
+
+const columnHelper = createColumnHelper<Row>();
+
 const DatabaseView = () => {
-  const data = useMemo(
+  const data: Row[] = useMemo(
     () => [
-      { id: 1, name: "Nguyễn Văn A", age: 25 },
-      { id: 2, name: "Trần Thị B", age: 30 },
+      { id: "1", properties: { c1: "Nguyễn Văn A" } },
+      { id: "2", properties: { c1: "Nguyễn Văn B" } },
+    ],
+    [],
+  );
+
+  const columnSchema: Column[] = useMemo(
+    () => [
+      { id: "c1", name: "Name", type: "text" },
+      { id: "c3", name: "Deadline", type: "date" },
+      { id: "c4", name: "Done", type: "checkbox" },
     ],
     [],
   );
 
   const columns = useMemo(
     () => [
-      {
-        header: "ID",
-        accessorKey: "id",
-      },
-      {
-        header: "Họ và Tên",
-        accessorKey: "name",
-      },
-      {
-        header: "Tuổi",
-        accessorKey: "age",
-      },
+      ...columnSchema.map((schema) =>
+        columnHelper.accessor((row) => row.properties[schema.id], {
+          id: schema.id,
+          header: schema.name,
+          cell: (data) => data.getValue(),
+        }),
+      ),
+
+      columnHelper.display({
+        id: "_add",
+        header: () => <button>+</button>,
+        cell: () => (
+          <button>
+            <Trash />
+          </button>
+        ),
+      }),
     ],
-    [],
+    [columnSchema],
   );
 
   const table = useReactTable({
@@ -51,18 +88,12 @@ const DatabaseView = () => {
       </div>
 
       <div className="p-4">
-        <table
-          className="border"
-          style={{ width: "100%", borderCollapse: "collapse" }}
-        >
+        <table className="w-full border-collapse border">
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    style={{ padding: "8px", background: "#f4f4f4" }}
-                  >
+                  <th key={header.id} className="bg-[#f4f4f4] p-2">
                     {flexRender(
                       header.column.columnDef.header,
                       header.getContext(),
@@ -77,7 +108,7 @@ const DatabaseView = () => {
             {table.getRowModel().rows.map((row) => (
               <tr key={row.id}>
                 {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} style={{ padding: "8px" }}>
+                  <td key={cell.id} className="p-2">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </td>
                 ))}
