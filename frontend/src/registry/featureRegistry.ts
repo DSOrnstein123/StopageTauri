2fb795bdf78f6d350b987412e37e72a5fc598ddf
DSOrnstein <namphonggithub@gmail.com>
2026-04-13 11:26:44 +0700

    feat(fe): redefine feature type

--- frontend/src/registry/featureRegistry.ts
+++ frontend/src/registry/featureRegistry.ts
@@ -1,18 +1,17 @@
+import Canvas from "@/features/canvas/Canvas";
 import DocumentView from "@/features/document/DocumentView";
 import Calendar from "@/features/planner/components/Calendar";
 import { Calendar1 } from "lucide-react";
 import type { ComponentType, ElementType } from "react";
 
-export type FeatureType = "document" | "planner";
-
 interface FeatureDefinition {
-  type: FeatureType;
+  type: string;
   icon?: ElementType;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   component?: ComponentType<any>;
 }
 
-export const featureRegistry: Record<FeatureType, FeatureDefinition> = {
+export const featureRegistry = {
   document: {
     type: "document",
     component: DocumentView,
@@ -22,4 +21,10 @@ export const featureRegistry: Record<FeatureType, FeatureDefinition> = {
     icon: Calendar1,
     component: Calendar,
   },
-};
+  canvas: {
+    type: "canvas",
+    component: Canvas,
+  },
+} as const satisfies Record<string, FeatureDefinition>;
+
+export type FeatureType = keyof typeof featureRegistry;
