0b33b56169e6d7f5710f2bf45fa47f4d104173ad
DSOrnstein <namphonggithub@gmail.com>
2026-04-16 21:08:24 +0700

    refactor(fe/document): split mutation logic into new hook file

---
+++ frontend/src/features/document/hooks/useAddProperty.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api/core";
+import collectionKeys from "@/features/document/keys/collectionKeys";
+import type {
+  Collection,
+  ColumnSchema,
+} from "../tiptap/extensions/collection/collection.types";
+
+export interface AddPropertyParams {
+  name: string;
+  type: ColumnType;
+}
+
+export const useAddProperty = (collectionId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (params: AddPropertyParams) =>
+      invoke<ColumnSchema>("add_property", {
+        collectionId: collectionId,
+        name: params.name,
+        propertyType: params.type,
+      }),
+    onSuccess: (data: ColumnSchema) => {
+      queryClient.setQueryData<Collection>(
+        collectionKeys.detail(collectionId),
+        (oldData) =>
+          oldData && { ...oldData, schema: [...oldData.schema, data] },
+      );
+    },
+  });
+};
--- frontend/src/features/document/tiptap/extensions/collection/AddColumnButton.tsx
+++ frontend/src/features/document/tiptap/extensions/collection/AddColumnButton.tsx
@@ -4,38 +4,11 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@/shared/components/shadcn/popover";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { invoke } from "@tauri-apps/api/core";
-import {
-  COLUMN_TYPES,
-  type Collection,
-  type ColumnSchema,
-  type ColumnType,
-} from "./collection.types";
-import collectionKeys from "@/features/document/hooks/collectionKeys";
-
-interface AddPropertyParams {
-  name: string;
-  type: ColumnType;
-}
+import { COLUMN_TYPES } from "./collection.types";
+import { useAddProperty } from "@/features/document/hooks/useAddProperty";
 
 const AddColumnButton = ({ collectionId }: { collectionId: string }) => {
-  const queryClient = useQueryClient();
-  const { mutate: addProperty } = useMutation({
-    mutationFn: (params: AddPropertyParams) =>
-      invoke<ColumnSchema>("add_property", {
-        collectionId: collectionId,
-        name: params.name,
-        propertyType: params.type,
-      }),
-    onSuccess: (data: ColumnSchema) => {
-      queryClient.setQueryData<Collection>(
-        collectionKeys.detail(collectionId),
-        (oldData) =>
-          oldData && { ...oldData, schema: [...oldData.schema, data] },
-      );
-    },
-  });
+  const { mutate: addProperty } = useAddProperty(collectionId);
 
   return (
     <Popover>
