41b7f709585572216c658d3d494bcab040d55755
DSOrnstein <namphonggithub@gmail.com>
2026-04-17 22:53:58 +0700

    feat(be): add folders and files table

---
+++ backend/src/database/migrations/20260417142228_add_folders_and_files_table.sql
@@ -0,0 +1,24 @@
+-- Add migration script here
+CREATE TABLE folders (
+    id TEXT PRIMARY KEY,
+    parent_id TEXT,
+    icon TEXT NOT NULL CHECK (json_valid(icon)),
+    name TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_trashed BOOLEAN DEFAULT 0,
+    FOREIGN KEY (parent_id) REFERENCES folders (id) ON DELETE CASCADE
+)
+
+CREATE TABLE files (
+    id TEXT PRIMARY KEY,
+    folder_id TEXT,
+    icon TEXT NOT NULL CHECK (json_valid(icon)),
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    content_id TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_trashed BOOLEAN DEFAULT 0,
+    FOREIGN KEY (folder_id) REFERENCES folders (id) ON DELETE CASCADE
+)
