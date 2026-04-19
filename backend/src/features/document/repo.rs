d66b5a047c20cd51f9c45a75cd52638f78e643fe
DSOrnstein <namphonggithub@gmail.com>
2026-04-19 12:51:05 +0700

    refactor(document): apply transaction to create both file and document in create_document api

--- backend/src/features/document/repo.rs
+++ backend/src/features/document/repo.rs
@@ -1,17 +1,16 @@
+use crate::entities::file::models::{File, IconData};
+use crate::features::document::models::{Document, DocumentContent, DocumentInCollection};
 use sqlx::types::Json;
 use sqlx::{Error, SqlitePool, query, query_as};
 use std::collections::HashMap;
 use uuid::Uuid;
-
-use crate::features::document::models::{Document, DocumentContent, DocumentInCollection};
 
 pub async fn get_document_list(pool: &SqlitePool) -> Result<Vec<Document>, Error> {
     let documents = query_as!(
         Document,
         r#"
         SELECT
-            id as "id!: String",
-            title
+            id as "id!: String"
         FROM documents
         "#
     )
@@ -44,26 +43,50 @@ pub async fn get_documents_in_collection(
     Ok(documents)
 }
 
-pub async fn create_document(pool: &SqlitePool) -> Result<Document, Error> {
-    let id = Uuid::new_v4().simple().to_string();
-    let title = "Untitled";
+pub async fn create_document(pool: &SqlitePool) -> Result<File, Error> {
+    let mut tx = pool.begin().await?;
 
-    let document = query_as!(
+    let document_id = Uuid::new_v4().simple().to_string();
+    query_as!(
         Document,
         r#"
-            INSERT INTO documents (id, title)
-            VALUES (?, ?)
-            RETURNING
-                id as "id!: String",
-                title
+            INSERT INTO documents (id)
+            VALUES (?)
         "#,
-        id,
-        title
+        document_id
     )
-    .fetch_one(pool)
+    .execute(&mut *tx)
     .await?;
 
-    Ok(document)
+    let file_id = Uuid::new_v4().simple().to_string();
+    let icon = Json(IconData {
+        icon_type: "lucide".to_string(),
+        value: "file-text".to_string(),
+    });
+    let document_file = query_as!(
+        File,
+        r#"
+            INSERT INTO files (id, name, icon, type, content_id)
+            VALUES (?, 'Untitled', ?, 'document', ?)
+            RETURNING
+                id as "id!: String",
+                name,
+                icon as "icon: Json<IconData>",
+                type as file_type,
+                content_id,
+                created_at,
+                updated_at
+        "#,
+        file_id,
+        icon,
+        document_id
+    )
+    .fetch_one(&mut *tx)
+    .await?;
+
+    tx.commit().await?;
+
+    Ok(document_file)
 }
 
 pub async fn update_document(pool: &SqlitePool, id: String, content: String) -> Result<(), Error> {
