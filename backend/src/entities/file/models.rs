9855b3034ab4dda3adf23367b5e47c201bbe4eb6
DSOrnstein <namphonggithub@gmail.com>
2026-04-19 12:44:26 +0700

    feat(be/app/file): add get_files api

---
+++ backend/src/entities/file/models.rs
@@ -0,0 +1,24 @@
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use sqlx::types::Json;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct File {
+    pub id: String,
+    pub name: String,
+    pub icon: Json<IconData>,
+    #[serde(rename = "type")]
+    pub file_type: String,
+    pub content_id: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IconData {
+    #[serde(rename = "type")]
+    pub icon_type: String,
+    pub value: String,
+}
--- backend/src/entities/file/repo.rs
+++ backend/src/entities/file/repo.rs
@@ -1,1 +1,26 @@
+use anyhow::{Error, Ok};
+use sqlx::{SqlitePool, query_as};
 
+use crate::entities::file::models::{File, IconData};
+use sqlx::types::Json;
+
+pub async fn get_files(pool: &SqlitePool) -> Result<Vec<File>, Error> {
+    let files = query_as!(
+        File,
+        r#"
+            SELECT 
+              id as "id!: String",
+              name,
+              icon as "icon: Json<IconData>",
+              type as file_type,
+              content_id,
+              created_at,
+              updated_at
+            FROM files
+        "#
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(files)
+}
