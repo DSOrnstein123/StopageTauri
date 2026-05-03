aea23f5c005b01e2d7730fb4ab72e81940336a1d
DSOrnstein <namphonggithub@gmail.com>
2026-05-03 22:59:26 +0700

    feat(fe/app): implement class initialization

---
+++ frontend/src/app/init.ts
@@ -0,0 +1,5 @@
+import { FeatureRegistry } from "@/core/registries/featureRegistry";
+import { QueryClient } from "@tanstack/react-query";
+
+export const featureRegistry = new FeatureRegistry();
+export const queryClient = new QueryClient();
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./app/init";
+import "@/features/document/index";
 import App from "./App.tsx";
 import "./index.css";
 import "./features/document/tiptap/tiptap.css";
 import "highlight.js/styles/github-dark.css";
 import "dockview/dist/styles/dockview.css";
-import "./layout/dockview/custom-dockview.css";
+import "./core/layout/dockview/custom-dockview.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
