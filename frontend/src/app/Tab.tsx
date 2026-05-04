75f9c245726756f297432fc7dbb31a4516b223e0
DSOrnstein <namphonggithub@gmail.com>
2026-05-04 23:26:23 +0700

    feat(fe/app): implement tab logic

---
+++ frontend/src/app/Tab.tsx
@@ -0,0 +1,35 @@
+import TabProvider from "@/shared/tab-context/TabProvider";
+import File from "@entities/file/components/File";
+import type { IDockviewPanelProps } from "dockview-core";
+import { useEffect, useState } from "react";
+import { resolveComponent } from "./resolveComponent";
+
+interface TabParams {
+  id: string;
+}
+
+const Tab = (props: IDockviewPanelProps<TabParams>) => {
+  const [isActive, setIsActive] = useState(props.api.isActive);
+
+  useEffect(() => {
+    const disposable = props.api.onDidActiveChange((event) => {
+      setIsActive(event.isActive);
+    });
+
+    return () => disposable.dispose();
+  }, [props.api]);
+
+  const value = {
+    id: props.api.id,
+    isActive: isActive,
+    setTitle: (title: string) => props.api.setTitle(title),
+  };
+
+  return (
+    <TabProvider props={value}>
+      <File id={props.params.id} resolveComponent={resolveComponent} />
+    </TabProvider>
+  );
+};
+
+export default Tab;
--- frontend/src/core/layout/dockview/panel-context/panelContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import type { IDockviewPanelProps } from "dockview-core";
-
-export const PanelContext = createContext<IDockviewPanelProps<
-  Record<string, unknown>
-> | null>(null);
