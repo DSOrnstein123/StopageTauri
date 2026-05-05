f5da80dcc503ec45afa5efb1eae4e9cc72417cf3
DSOrnstein123 <namphonggithub@gmail.com>
2026-05-05 11:26:31 +0700

    feat(fe/shared): implement sidebar store

---
+++ frontend/src/shared/useSidebarStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface ISidebarStore {
+  type: string;
+
+  setType: (type: string) => void;
+}
+
+const createSidebarStore = () =>
+  create<ISidebarStore>((set) => ({
+    type: "",
+
+    setType: (type) => set({ type: type }),
+  }));
+
+export const useRightSidebarStore = createSidebarStore();
+export const useLeftSidebarStore = createSidebarStore();
