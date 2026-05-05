c16e4bb1acda542d969d5c990472a33547042541
DSOrnstein123 <namphonggithub@gmail.com>
2026-05-05 11:23:46 +0700

    feat(fe/document): implement document public api

--- frontend/src/features/document/index.ts
+++ frontend/src/features/document/index.ts
@@ -1,10 +1,2 @@
-import { featureRegistry } from "@/app/init";
-import Document from "./components/Document";
-import { DocumentFileSchema } from "./schemas/documentSchema";
-import { documentService } from "./services/documentService";
-
-featureRegistry.register("document", {
-  component: Document,
-  parser: DocumentFileSchema.parse,
-  fetcher: (id: string) => documentService.getDetail(id),
-});
+export { default as Document } from "./components/Document";
+export { default as DocumentSidebar } from "./components/DocumentSidebar";
