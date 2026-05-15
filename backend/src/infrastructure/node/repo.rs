b8f76c46544a508c6bc549f019cfe20ced618d7d
DSOrnstein123 <namphonggithub@gmail.com>
2026-05-15 11:44:09 +0700

    refactor(be/infrastructure): update api to align with domain

--- backend/src/infrastructure/node/repo.rs
+++ backend/src/infrastructure/node/repo.rs
@@ -1,43 +1,58 @@
+use crate::domain::models::icon_data::IconData;
+use crate::domain::models::node::{NodeDetail, NodeFilterOptions, NodeKind, NodeMetadata};
+use crate::domain::ports::node_repository::NodeRepository;
+use crate::infrastructure::node::models::{DbNodeDetail, DbNodeMetadata};
 use anyhow::{Error, Ok};
-use sqlx::{QueryBuilder, Sqlite, SqlitePool, query, query_as};
-
-use crate::infrastructure::node::models::{IconData, Node, NodeDetail, NodeFilterOptions};
+use async_trait::async_trait;
 use serde_json::Value;
 use sqlx::types::Json;
+use sqlx::{QueryBuilder, Sqlite, SqlitePool, query, query_as};
 
-pub async fn get_node_detail(pool: &SqlitePool, id: String) -> Result<NodeDetail, Error> {
-    let node = query_as!(
-        NodeDetail,
-        r#"
-            SELECT 
-              id as "id!: String",
-              parent_id,
-              icon as "icon: Json<IconData>",
-              name,
-              kind,
-              type as node_type,
-              content as "content: Json<Value>",
-              properties as "properties: Json<Value>",
-              created_at,
-              updated_at,
-              is_trashed
-            FROM nodes
-            WHERE id = ? AND is_trashed = 0
-        "#,
-        id
-    )
-    .fetch_one(pool)
-    .await?;
-
-    Ok(node)
+pub struct SqliteNodeRepository {
+    pub pool: SqlitePool,
 }
 
-pub async fn get_nodes(
-    pool: &SqlitePool,
-    options: Option<NodeFilterOptions>,
-) -> Result<Vec<Node>, Error> {
-    let mut builder: QueryBuilder<Sqlite> = QueryBuilder::new(
-        r#"
+impl SqliteNodeRepository {
+    pub fn new(pool: SqlitePool) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl NodeRepository for SqliteNodeRepository {
+    async fn get_detail(&self, id: &str) -> Result<NodeDetail, Error> {
+        let db_node = query_as!(
+            DbNodeDetail,
+            r#"
+              SELECT 
+                id as "id!: String",
+                parent_id,
+                icon as "icon: Json<IconData>",
+                name,
+                kind as "kind: NodeKind",
+                type as node_type,
+                content as "content: Json<Value>",
+                properties as "properties: Json<Value>",
+                created_at,
+                updated_at,
+                is_trashed
+              FROM nodes
+              WHERE id = ? AND is_trashed = 0
+            "#,
+            id
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(db_node.into())
+    }
+
+    async fn get_list(
+        &self,
+        options: Option<NodeFilterOptions>,
+    ) -> Result<Vec<NodeMetadata>, Error> {
+        let mut builder: QueryBuilder<Sqlite> = QueryBuilder::new(
+            r#"
         SELECT 
             id,
             parent_id,
@@ -52,75 +67,75 @@ pub async fn get_nodes(
         FROM nodes
         WHERE is_trashed = 0
         "#,
-    );
+        );
 
-    if let Some(opts) = options {
-        if let Some(kinds) = opts.include_kinds {
-            if !kinds.is_empty() {
-                builder.push(" AND kind IN (");
-                let mut separated = builder.separated(", ");
-                for k in kinds {
-                    separated.push_bind(k);
+        if let Some(opts) = options {
+            if let Some(kinds) = opts.include_kinds {
+                if !kinds.is_empty() {
+                    builder.push(" AND kind IN (");
+                    let mut separated = builder.separated(", ");
+                    for k in kinds {
+                        separated.push_bind(k);
+                    }
+                    separated.push_unseparated(")");
                 }
-                separated.push_unseparated(")");
+            }
+
+            if let Some(kinds) = opts.exclude_kinds {
+                if !kinds.is_empty() {
+                    builder.push(" AND kind NOT IN (");
+                    let mut separated = builder.separated(", ");
+                    for k in kinds {
+                        separated.push_bind(k);
+                    }
+                    separated.push_unseparated(")");
+                }
+            }
+
+            if let Some(types) = opts.include_types {
+                if !types.is_empty() {
+                    builder.push(" AND type IN (");
+                    let mut separated = builder.separated(", ");
+                    for t in types {
+                        separated.push_bind(t);
+                    }
+                    separated.push_unseparated(")");
+                }
+            }
+
+            if let Some(types) = opts.exclude_types {
+                if !types.is_empty() {
+                    builder.push(" AND type NOT IN (");
+                    let mut separated = builder.separated(", ");
+                    for t in types {
+                        separated.push_bind(t);
+                    }
+                    separated.push_unseparated(")");
+                }
             }
         }
 
-        if let Some(kinds) = opts.exclude_kinds {
-            if !kinds.is_empty() {
-                builder.push(" AND kind NOT IN (");
-                let mut separated = builder.separated(", ");
-                for k in kinds {
-                    separated.push_bind(k);
-                }
-                separated.push_unseparated(")");
-            }
-        }
+        let nodes = builder
+            .build_query_as::<DbNodeMetadata>()
+            .fetch_all(&self.pool)
+            .await?;
 
-        if let Some(types) = opts.include_types {
-            if !types.is_empty() {
-                builder.push(" AND type IN (");
-                let mut separated = builder.separated(", ");
-                for t in types {
-                    separated.push_bind(t);
-                }
-                separated.push_unseparated(")");
-            }
-        }
-
-        if let Some(types) = opts.exclude_types {
-            if !types.is_empty() {
-                builder.push(" AND type NOT IN (");
-                let mut separated = builder.separated(", ");
-                for t in types {
-                    separated.push_bind(t);
-                }
-                separated.push_unseparated(")");
-            }
-        }
+        Ok(nodes.into_iter().map(Into::into).collect())
     }
 
-    let nodes = builder.build_query_as::<Node>().fetch_all(pool).await?;
-
-    Ok(nodes)
-}
-
-pub async fn update_node_name(
-    pool: &SqlitePool,
-    id: String,
-    new_name: String,
-) -> Result<(), Error> {
-    query!(
-        r#"
+    async fn update_name(&self, id: &str, new_name: &str) -> Result<(), Error> {
+        query!(
+            r#"
             UPDATE nodes
             SET name = ?
             WHERE id = ? 
         "#,
-        new_name,
-        id,
-    )
-    .execute(pool)
-    .await?;
+            new_name,
+            id,
+        )
+        .execute(&self.pool)
+        .await?;
 
-    Ok(())
+        Ok(())
+    }
 }
