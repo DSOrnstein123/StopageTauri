63c80bb94472104839e68201c6a28ef58ac40070
DSOrnstein123 <namphonggithub@gmail.com>
2026-05-19 11:33:33 +0700

    feat(fe): add eslint rule to prevent deep import between modules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +20,47 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  {
+    files: ["src/plugins/**/*.{ts,tsx}", "src/core-plugins/**/*.{ts,tsx}"],
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: [
+                "@plugins/*/*",
+                "../plugins/*/*",
+                "../../plugins/*/*",
+                "@core-plugins/*/*",
+                "../core-plugins/*/*",
+                "../../core-plugins/*/*",
+              ],
+              message: "No deep import.",
+            },
+            {
+              group: ["@system/*/*"],
+              message: "No deep import.",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ["src/system/**/*.{ts,tsx}"],
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: ["@plugins/*/*", "@core-plugins/*/*"],
+              message: "No deep import.",
+            },
+          ],
+        },
+      ],
+    },
+  },
+]);
