4b26c89b45deb683df7634dfa67eb0cc6a7c7a56
DSOrnstein <namphonggithub@gmail.com>
2026-07-13 10:58:13 +0700

    refactor(fe/system): remove params from flush

--- frontend/src/system/utils/debounce.ts
+++ frontend/src/system/utils/debounce.ts
@@ -1,19 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const debounce = <T extends (...args: any[]) => void>(fn: T, delay: number) => {
   let timer: ReturnType<typeof setTimeout>;
+  let lastArgs: Parameters<T>;
 
   const debounced = function (this: unknown, ...args: Parameters<T>) {
+    lastArgs = args;
     clearTimeout(timer);
     timer = setTimeout(() => {
-      fn.apply(this, args);
+      fn.apply(this, lastArgs);
     }, delay);
   };
 
   debounced.cancel = () => clearTimeout(timer);
 
-  debounced.flush = (...args: Parameters<T>) => {
-    clearTimeout(timer);
-    fn(...args);
+  debounced.flush = () => {
+    debounced.cancel();
+    fn(...lastArgs);
   };
 
   return debounced;
